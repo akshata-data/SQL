5c368eda91889d196621c6236fc1cff86de3f449
Akshata A@ <107637257+akshata-data@users.noreply.github.com>
2026-07-14 20:34:58 +0530

    Update File1.py

--- EASY/File1.py
+++ EASY/File1.py
@@ -1,3 +1,9 @@
+select product_id from products where low_fats  like "Y" and recyclable  like "Y";
+select name from customer where referee_id not like 2 or referee_id is null;
+select name, population, area from world where population >=25000000 or area >= 3000000;
+select distinct author_id as id from views where author_id = viewer_id order by id;
+select tweet_id from tweets where length(content)>15;
+
 select id from (select *, lag(temperature) over (order by id) as lag_prev from weather) abc where temperature> lag_prev;
 
 /*select customer_id, count(customer_id) count_no_trans from(select a.customer_id, b.transaction_id from visits a left join transactions b on a.visit_id = b.visit_id) abc
